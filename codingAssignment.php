94a3e35c59cc530cb03d20e5bd1e3fc5065ae031
bford21 <fordb1@wit.edu>
2015-08-13 22:44:19 -0400

    Movies in theaters
    Queries myapifilms for list of current movies and prints them in a list

--- codingAssignment.php
+++ codingAssignment.php
@@ -5,5 +5,16 @@ Xamarin Coding Assignment
 This program lists the movies currently out in theaters and the average age of the cast.
 */
 
+// Queries myapifilms for movies currently in theaters
+$requestURL = "http://www.myapifilms.com/imdb/inTheaters?format=XML&lang=en-us";
+$xml = simplexml_load_string(file_get_contents($requestURL)) or die("Error: Cannot create object");
+	
+echo "<h2>Movies in Theaters</h2>";
+$totalMovies = count($xml->movies[1]);
 
+//Prints the list of movies currently in theaters
+for ($x = 0; $x <= $totalMovies; $x++) {
+	echo $xml->movies[1]->movie[$x]->title;
+	echo "<br>";
+}
 ?>
