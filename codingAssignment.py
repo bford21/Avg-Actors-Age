f446b0ec5b8abbdde13bbe7678e25f042ee9bd4e
bford21 <fordb1@wit.edu>
2015-08-14 20:38:18 -0400

    Added function to determine age

--- codingAssignment.py
+++ codingAssignment.py
@@ -2,8 +2,20 @@
 # Xamarin Coding Assignment
 # Lists current movies in theaters and displays avg age of cast
 
-import urllib
+import urllib, time, string
+from datetime import date
 from xml.etree import ElementTree as ET
+
+#function that calculates age based off dob passed in params
+def findAge(dob):
+  today = date.today()
+  
+  #Filters string of all known ASCII characters and replaces with ' '
+  dob = ''.join([i if ord(i) < 128 else ' ' for i in dob])
+  
+  #Parses the string representing the dob into a workable date format dd/MONTH/YEAR
+  born = time.strptime(dob, "%d %B %Y")
+  return today.year - born.tm_year - ((today.month, today.day) < (born.tm_mon, born.tm_mday))
 
 #Queries myapifilms for movies in theaters and returns result in XML format
 movieRequest = 'http://www.myapifilms.com/imdb/inTheaters?format=XML&lang=en-us'
@@ -22,21 +34,36 @@ for item in movies:
   
 
 #Iterates though all movies based on IMDB ID 
-for i in idIMDB:
-  print '\nMOVIE ID: ' + i 
+#for i in idIMDB:
+  #print '\nMOVIE ID: ' + i 
   
   #Queries myapifilms for actors based on movie ID passed to it and returns in XML format
-  actorRequest = 'http://www.myapifilms.com/imdb?idIMDB='+i+'&format=XML&aka=0&business=0&seasons=0&seasonYear=0&technical=0&lang=en-us&actors=S&biography=1&trailer=0&uniqueName=0&filmography=0&bornDied=1&starSign=0&actorActress=0&actorTrivia=0&movieTrivia=0&awards=0&moviePhotos=N&movieVideos=N&similarMovies=0'
-  root = ET.parse(urllib.urlopen(actorRequest)).getroot()
-  actors = root.findall('actors/actor')
-  dob = root.findall('actors/actor/biography/dateOfBirth')
+  #actorRequest = 'http://www.myapifilms.com/imdb?idIMDB='+i+'&format=XML&aka=0&business=0&seasons=0&seasonYear=0&technical=0&lang=en-us&actors=S&biography=1&trailer=0&uniqueName=0&filmography=0&bornDied=1&starSign=0&actorActress=0&actorTrivia=0&movieTrivia=0&awards=0&moviePhotos=N&movieVideos=N&similarMovies=0'
+actorRequest = 'http://www.myapifilms.com/imdb?idIMDB=tt3152624&format=XML&aka=0&business=0&seasons=0&seasonYear=0&technical=0&lang=en-us&actors=S&biography=1&trailer=0&uniqueName=0&filmography=0&bornDied=1&starSign=0&actorActress=0&actorTrivia=0&movieTrivia=0&awards=0&moviePhotos=N&movieVideos=N&similarMovies=0'
+root = ET.parse(urllib.urlopen(actorRequest)).getroot()
+actors = root.findall('actors/actor')
+
+#Array to hold ages
+ages = []
+
+#Lists all actors
+for item in actors:
+  print item.find('actorName').text.encode('utf-8')
+  dob = item.find('biography/dateOfBirth')
+  if(dob == None):
+    print dob
+  else:
+    bday = dob.text.encode('utf-8')
+    age = findAge(bday)
+    ages.append(age)
+    print age
+
+total = 0
+for i in ages:
+  total += i
+
+avg = total/len(ages)
+print 'AVERAGE AGE: ' + str(avg)
   
-  #Lists all actors
-  for item in actors:
-    print item.find('actorName').text.encode('utf-8')
-    
-  #lists actors dob
-  for item in dob:
-    print item.text.encode('utf-8')
 
   
