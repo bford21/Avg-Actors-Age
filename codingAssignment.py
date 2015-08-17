24cd1f7bbe59cbd7ebf4f0e02f97c5584fb4030b
bford21 <fordb1@wit.edu>
2015-08-16 21:57:28 -0400

    Formatted output

--- codingAssignment.py
+++ codingAssignment.py
@@ -13,57 +13,64 @@ def findAge(dob):
   #Filters string of all known ASCII characters and replaces with ' '
   dob = ''.join([i if ord(i) < 128 else ' ' for i in dob])
   
-  #Parses the string representing the dob into a workable date format dd/MONTH/YEAR
-  born = time.strptime(dob, "%d %B %Y")
-  return today.year - born.tm_year - ((today.month, today.day) < (born.tm_mon, born.tm_mday))
+  #determines if dob contains full birthday or just year
+  if ' ' in dob:
+    #Parses the string representing the dob into a workable date format dd/MONTH/YEAR
+    born = time.strptime(dob, "%d %B %Y")
+    return today.year - born.tm_year - ((today.month, today.day) < (born.tm_mon, born.tm_mday))
+  else:
+    #filters string of any non digits
+    dob = filter(lambda x: x.isdigit(), dob)
+    born = time.strptime(dob, "%Y")
+    return today.year - born.tm_year
+
+#Array declarations
+idIMDB = []
+titles = []
+ages = []
 
 #Queries myapifilms for movies in theaters and returns result in XML format
 movieRequest = 'http://www.myapifilms.com/imdb/inTheaters?format=XML&lang=en-us'
 root = ET.parse(urllib.urlopen(movieRequest)).getroot()
-movies = root.findall('movies/movie')
-
-#Array to hold movie ID's for easy look up
-idIMDB = []
+movies = root.findall('movies[0]/movie')
 
 #Iterates through all movies in theaters and prints title
 print 'MOVIES IN THEATERS'
 for item in movies:
   movieID = item.find('idIMDB').text
   idIMDB.append(movieID)
-  print item.find('title').text.encode('utf-8') + ' - ' + movieID
+  title = item.find('title').text.encode('utf-8')
+  titles.append(title)
+  print title
+
+#Variable used to keep track of current movie 
+x = 0;
+
+#Iterates though all movies based on IMDB ID
+for i in idIMDB:
+  print '\nMOVIE: ' + titles[x]
+  x += 1
   
-
-#Iterates though all movies based on IMDB ID 
-#for i in idIMDB:
-  #print '\nMOVIE ID: ' + i 
+  #Queries myapifilms for actors based on movie ID and returns results in XML format
+  actorRequest = 'http://www.myapifilms.com/imdb?idIMDB='+i+'&format=XML&aka=0&business=0&seasons=0&seasonYear=0&technical=0&lang=en-us&actors=S&biography=1&trailer=0&uniqueName=0&filmography=0&bornDied=1&starSign=0&actorActress=0&actorTrivia=0&movieTrivia=0&awards=0&moviePhotos=N&movieVideos=N&similarMovies=0'  
+  root = ET.parse(urllib.urlopen(actorRequest)).getroot()
+  actors = root.findall('actors/actor')
   
-  #Queries myapifilms for actors based on movie ID passed to it and returns in XML format
-  #actorRequest = 'http://www.myapifilms.com/imdb?idIMDB='+i+'&format=XML&aka=0&business=0&seasons=0&seasonYear=0&technical=0&lang=en-us&actors=S&biography=1&trailer=0&uniqueName=0&filmography=0&bornDied=1&starSign=0&actorActress=0&actorTrivia=0&movieTrivia=0&awards=0&moviePhotos=N&movieVideos=N&similarMovies=0'
-actorRequest = 'http://www.myapifilms.com/imdb?idIMDB=tt3152624&format=XML&aka=0&business=0&seasons=0&seasonYear=0&technical=0&lang=en-us&actors=S&biography=1&trailer=0&uniqueName=0&filmography=0&bornDied=1&starSign=0&actorActress=0&actorTrivia=0&movieTrivia=0&awards=0&moviePhotos=N&movieVideos=N&similarMovies=0'
-root = ET.parse(urllib.urlopen(actorRequest)).getroot()
-actors = root.findall('actors/actor')
-
-#Array to hold ages
-ages = []
-
-#Lists all actors
-for item in actors:
-  print item.find('actorName').text.encode('utf-8')
-  dob = item.find('biography/dateOfBirth')
-  if(dob == None):
-    print dob
-  else:
-    bday = dob.text.encode('utf-8')
-    age = findAge(bday)
-    ages.append(age)
-    print age
-
-total = 0
-for i in ages:
-  total += i
-
-avg = total/len(ages)
-print 'AVERAGE AGE: ' + str(avg)
+  #Lists all actors and their age
+  for item in actors:
+    dob = item.find('biography/dateOfBirth')
+    if(dob != None):
+      bday = dob.text.encode('utf-8')
+      age = findAge(bday)
+      ages.append(age)
+      print item.find('actorName').text.encode('utf-8') + ' ' + str(age)
+  
+  total = 0
+  for i in ages:
+    total += i
+  
+  print 'AVERAGE AGE: ' + str(total/len(ages))
+  ages = []
   
 
   
